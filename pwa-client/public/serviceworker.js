ccd99248310c5fe7f27128987f971b06282e6df4
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-09 00:24:36 -0400

    added event listener for installation of service worker

--- pwa-client/public/serviceworker.js
+++ pwa-client/public/serviceworker.js
@@ -2,3 +2,14 @@
 const CACHE_NAME = "version-1";
 const URLsToCache = ['index.html', "offline.html"];
 const self = this;
+
+// service worker installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache) => {
+      console.log('Opened Cache');
+      return cache.addAll(URLsToCache);
+    })
+  );
+});
